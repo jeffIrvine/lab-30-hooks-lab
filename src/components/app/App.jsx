672a0b0e80fee3fea0f8797a42df85d71cf4581d
Your NamejeffIrvine <jirvine1212@gmail.com>
2021-02-08 21:59:32 -0800

    created app function comp with router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,26 @@
+/* eslint-disable max-len */
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import MainContainer from '../containers/main/MainContainer';
+import DetailContainer from '../containers/detail/DetailContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route
+          exact path="/"
+          component = {MainContainer}
+        />
+        <Route
+          path="/detail/:id"
+          component = {DetailContainer}
+        />
+      </Switch>
+    </Router>
+  );
 }
